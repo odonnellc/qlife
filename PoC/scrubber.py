a5ce35bee174d46b8c2f0e9bcdd17044279af694
odonnellc <cod745@gmail.com>
2015-02-10 21:37:28 +0000

    Adding comments
    Adding comments

--- PoC/scrubber.py
+++ PoC/scrubber.py
@@ -1,6 +1,11 @@
+#This is a proof of concept and does not reflect the final product
+
 import requests
 
+#Grab the library seating
 request = requests.get('http://pcs.qub.ac.uk/')
 
+
+#Output it to a file
 with open("output.txt", "wt") as out_file:
 	out_file.write(request.text);
